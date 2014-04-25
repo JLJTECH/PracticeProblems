35932a1ceb9375069aef390632730a7107662d62
JLJTECH <jljtechnologies@gmail.com>
2014-04-25 11:23:17 -0400

    changes

--- ArrayReduce.js
+++
@@ -1,7 +0,0 @@
-function sum(array) {
-  // Use array.reduce() to calculate and return the
-  // sum of the values in array.
-  return array.reduce(function(sum, value){
-    return sum + value;
-  });
-}
