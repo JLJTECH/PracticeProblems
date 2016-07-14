d7f7faacb1408d39183b6af9d02ec971a694ae64
jljtech <jljtechnologies@gmail.com>
2016-07-13 21:35:36 -0400

    Day 7

---
+++ hackerrank/30 Days of Code/Day/Arrays.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+#Reverse array and adjust output.
+
+import sys
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+for i in reversed(arr):
+    print (i, end=' ')
