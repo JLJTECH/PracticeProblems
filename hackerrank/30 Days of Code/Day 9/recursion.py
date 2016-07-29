1070bbb72ca586d725b5054dc097065b8a7b81a6
jljtech <jljtechnologies@gmail.com>
2016-07-29 13:03:43 -0400

    Day 4 & 9

--- hackerrank/30 Days of Code/Day 9/recursion.py
+++ hackerrank/30 Days of Code/Day 9/recursion.py
@@ -1,8 +1,10 @@
-#Recursion problem
+#Recursion problem - Task: Write a factorial function that takes a positive integer,  as a parameter and prints the result of  ( factorial).
+
 def factorial(n):
-    if n == 0 or n == 1:
+    if n <= 1:
         return 1
     else:
-        return n * factorial(n-1)
+        result = n * factorial(n - 1)
+        return result
 
 print(factorial(int(input())))
