25a23c5b942d581420c8326eb2714417a39dcded
jljtech <jljtechnologies@gmail.com>
2016-08-02 23:44:22 -0400

    Coding Bat solutions

---
+++ CodingBat/Python/Warmup-1 > near_hundred.py
@@ -0,0 +1,12 @@
+#Warmup-1 > near_hundred
+def near_hundred(n):
+  if n <=100 and n >=90:
+    return True
+  elif n <=110 and n >=100:
+    return True
+  elif n <=200 and n >=190:
+    return True
+  elif n <=210 and n >=200:
+    return True
+  else:
+    return False
