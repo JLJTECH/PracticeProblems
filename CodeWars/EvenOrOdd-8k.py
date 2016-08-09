02d99d49d4c3a73254709ad6f08191cebe81cbc0
jljtech <jljtechnologies@gmail.com>
2016-08-08 21:29:12 -0400

    Starting code wars challenges

---
+++ CodeWars/EvenOrOdd-8k.py
@@ -0,0 +1,8 @@
+def even_or_odd(number):
+    #Check for remainder
+    if number % 2 == 0:
+    #If remainder not present return Even
+        return 'Even'
+    #Otherwise return Odd
+    else:
+        return 'Odd'
