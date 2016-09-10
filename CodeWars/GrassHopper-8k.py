7be5a16089749815fbea02113d0a66351b309103
jljtech <jljtechnologies@gmail.com>
2016-09-09 23:31:10 -0400

    Codewars Solution

---
+++ CodeWars/GrassHopper-8k.py
@@ -0,0 +1,3 @@
+#Create a function named combineNames(combine_names in python, ruby) that accepts two parameters (first and last name). The function should return the full name.
+def combine_names(a,b):
+    return a +' '+ b
