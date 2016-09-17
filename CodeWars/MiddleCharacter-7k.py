8d58a73a05540c6bd7dcac65a09187e0ebc86f78
jljtech <jljtechnologies@gmail.com>
2016-09-16 22:18:00 -0400

    Codewars Solution

---
+++ CodeWars/MiddleCharacter-7k.py
@@ -0,0 +1,12 @@
+#Return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+def get_middle(s):
+    if len(s) % 2 == 0:
+        return s[int(len(s) / 2) - 1] + s[int((len(s) / 2))]
+    else:
+        return s[int(len(s) / 2)]
+
+
+#Alternate Solution
+def get_middle(s):
+    index, odd = divmod(len(s), 2)
+    return s[index] if odd else s[index - 1:index + 1]
