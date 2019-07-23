0c53ce4d89d3b842cc8dbd7b6ff4a5caa9736764
JT <jljtechnologies@gmail.com>
2019-07-22 22:59:47 -0400

    Added some updates and neat tools

--- Misc/reminders.py
+++ Misc/reminders.py
@@ -1,10 +1,13 @@
-#Python reminders - those pesky Gotchas!
+#Python reminders
 
-''.join(list) # Collapse the list
+# Collapse the list
+''.join(list) 
 
-[a,b,c,d,e].count(item) #Count occurrences of item in list
+#Count occurrences of item in list
+[a,b,c,d,e].count(item) 
 
-a[0] #put anything in square brackets to search list index
+#put anything in square brackets to search list index
+a[0] 
 
 #Strange list rotation
 def rotate_left3(nums):
@@ -19,3 +22,19 @@ if someting:
 	return (item 1 >= 10)
 else:
 	return (item > 1 and item <= 5)
+
+#Use F strings
+x = "hello"
+w = "world"
+print(f"{x} {w}")
+
+#List comprehensions
+	#example CW solution used to reverse all words >= 5 letters and print out the modified sentence
+	[word[::-1] if len(word) >= 5 else word for word in words]
+
+#Collapse multiple inputs into one
+	#from this
+	def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+	#to this
+	def predict_age(*ages)
+
