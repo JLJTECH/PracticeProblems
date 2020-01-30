06a3062df91a4e048dc348d6514ecbe5ed23e6be
JT <jljtechnologies@gmail.com>
2020-01-30 14:26:14 -0500

    Create Mi-Km.py

---
+++ Edabit/Mi-Km.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''
+Create a function to calculate miles when given kilometers to 5 decimal places.
+'''
+def km_to_miles(kilometers):
+	return round(kilometers * 0.621371, 5)
